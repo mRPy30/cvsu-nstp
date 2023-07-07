e39ba9603f17f8f4dec3bff31180e7e783dfb618
Janvier Erickson <janviererickson.araque@cvsu.edu.ph>
2023-07-07 19:37:58 +0800

    test

---
+++ login.php
@@ -0,0 +1,169 @@
+<?php
+// CONNECTION
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login_system";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['submit'])) {
+        $username = $_POST['id'];
+        $password = $_POST['password'];
+        $Result = mysqli_query($conn, "SELECT id, password, 'instructor' as role FROM instructor WHERE id ='$username' AND password='$password'
+		UNION
+		SELECT id, password, 'student' as role FROM student WHERE id ='$username' AND password='$password'
+		UNION
+		SELECT id, password, 'admin' as role FROM coordinator WHERE id ='$username' AND password='$password'");
+
+        if (!$Result) {
+            // Query execution failed
+            die("Query failed: " . mysqli_error($conn));
+        }
+
+        $matchedRows = mysqli_num_rows($Result);
+
+        if ($matchedRows > 0) {
+            $row = mysqli_fetch_assoc($Result);
+            $id = $row['id'];
+            $role = $row['role'];
+
+            $_SESSION['name'] = $username;
+            $_SESSION['id'] = $id;
+
+            if ($role == 'admin') {
+                Header("location: adminpage.php?id=$id");
+                exit();
+            } elseif ($role == 'student') {
+                Header("location: studentpage.php?id=$id");
+                exit();
+            } elseif ($role == 'instructor') {
+                Header("location: instructorpage.php?id=$id");
+                exit();
+            }
+        } else {
+            // No matching user found
+            echo ("no data matched");
+            exit();
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!---------TITLE-------------->
+    <link rel="short icon" href="logo.png" type="">
+    <title>LOGIN PAGE</title>
+    
+    <!---------CSS LINK------------>
+    <link rel="stylesheet" href="style_log-in_&_sign-up.css">
+   
+     <!----------BOOTSTRAP------------>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    <!--------FONT LINK----------->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abel&family=Inter:wght@500;800&family=Poppins:wght@400;600&display=swap"
+        rel="stylesheet">
+
+    <!--------ICONS--------->
+    <script src="https://kit.fontawesome.com/11a4f2cc62.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    <!------navbar------>
+    <nav class="navbar-login_signup login-header-section sticky-top  header-section-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 logo">
+                    <div class="p1">
+                        <p>NSTP PROGRAM</p>
+                    </div>
+                    <div class="col-4 home">
+                        <a href="student.php" class="home-link "><i class="fa-solid fa-house"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!------End navbar------>
+
+    <!-----main content----->
+    <div class="bg">
+        <img src="background-login.jpg" alt="cvsucover">
+    </div>
+
+    <div class="login-section login">
+        <div class="container logout_container">
+            <div class="row">
+                <div class=" col text-align ">
+                    <img src=logo.png>
+                    <div class="body_logo ">
+                        <p> CAVITE STATE UNIVERSITY</p>
+                    </div>
+                    <div class="body-title">
+                        <h1>IMUS CAMPUS</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+            <!--login section-->
+            <aside class="form-login">
+                <div class="container">
+                    <div class="box">   
+                        <div class="form-group">
+                            <div class="login-title">
+                                <h3>Login to your account</h3>
+                            </div>
+                            <form class="form-login needs-validation" method="POST">
+                                <label class="enter" for="id">Enter your ID:</label>
+                                <input type="text is-valid" class="form-control" placeholder="Enter your Id" name="id" required>
+                                <label class="pass log" for="password">Password:</label>
+                                <input type="password is-valid" class="form-control" placeholder="Enter your Password" name="password" id="password" required>
+                                <span class="password-toggle" onclick="togglePasswordVisibility()"><i class="fa-solid fa-eye"></i></span>
+                                <span class="error" id="passwordError"></span>
+                                    <button class="btn btn-lg btn-block" type="submit" name="submit" value="Submit">LOGIN</button>
+                            </form>
+                            <div class="signup">
+                                <p>Don't have an account yet? <a href="signup.php"> Signup Here </a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </aside>
+            <!--End login section-->
+
+            <!--password validation and view hide-->
+            <script>function togglePasswordVisibility() {
+                var passwordInput = document.getElementById("password");
+                var passwordToggle = document.querySelector(".password-toggle");
+
+                if (passwordInput.type === "password") {
+                    passwordInput.type = "text";
+                    passwordToggle.innerHTML = '<i class="fa-solid fa-eye"></i>';
+                    passwordToggle.querySelector("i").classList.add("active");
+                } else {
+                    passwordInput.type = "password";
+                    passwordToggle.innerHTML = '<i class="fa-solid fa-eye-slash"></i>';
+                }
+
+                }
+            </script>
+    <!----End main content----->
+</body>
+</html>
---
+++ sign.up.php
@@ -0,0 +1,406 @@
+<?php
+// Database configuration
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login_system";
+
+// Create a database connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check if the connection was successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+// Fetch course data from the database
+$query = "SELECT courseID, courseName FROM tbl_course";
+$result = mysqli_query($conn, $query);
+$courses = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// Check if the form is submitted
+if (isset($_POST['submit'])) {
+
+    // Retrieve the form data
+    $name = $_POST["name"];
+    $id = $_POST["id"];
+    $course = $_POST["course"];
+    $section = $_POST["sectionID"];
+    $password = $_POST["password"];
+
+    
+        // Prepare and execute the SQL statement to insert data into the student table
+        $sql = "INSERT INTO student (name, id, course, sectionID, password) VALUES ('$name', '$id', '$course', '$section', '$password')";
+        $query = mysqli_query($conn, $sql);
+
+    if($query){
+
+        ?>
+       <script>
+        $(function(){
+            $('#btn').click(function(e){
+    
+                var valid = this.form.checkValidity();
+    
+                if(valid){
+    
+    
+                var name     	= $('#name').val();
+                var idate   	= $('#id').val();
+                var course 		= $('#course').val();
+                var section     = $('#section').val();
+                var password 	= $('#password').val();
+                
+    
+                    e.preventDefault();	
+    
+                    $.ajax({
+                        type: 'POST',
+                        url: 'sign.up.php',
+                        data: {name: name,id: id,course: course,section: section,password: password},
+                        success: function(data){
+                        Swal.fire({
+                                    'title': 'Successful',
+                                    'text': data,
+                                    'type': 'success'
+                                    })
+                                
+                        },
+                        error: function(data){
+                            Swal.fire({
+                                    'title': 'Errors',
+                                    'text': 'There were errors while saving the data.',
+                                    'type': 'error'
+                                    })
+                        }
+                    });
+    
+                    
+                }else{
+                    
+                }
+    
+                
+    
+    
+    
+            });		
+    
+            
+        });
+        
+        </script>
+
+
+    <?php
+    }
+
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!---------title-------------->
+    <link rel="short icon" href="logo.png" type="">
+    <title>SIGN UP PAGE</title>
+
+    <!---------Css link------------>
+    <link rel="stylesheet" href="style_log-in_&_sign-up.css">
+
+    <!-----------bootsrap------------>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!--------font link----------->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Abel&family=Inter:wght@500;800&family=Poppins:wght@400;600&display=swap"
+        rel="stylesheet">
+
+    <!--------icons--------->
+    <script src="https://kit.fontawesome.com/11a4f2cc62.js" crossorigin="anonymous"></script>
+
+    <!-------popup modal js------>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+</head>
+
+<body>
+    <!------navbar------>
+    <nav class="navbar-login_signup login-header-section sticky-top header-section-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 logo">
+                    <div class="p1">
+                        <p>NSTP PROGRAM</p>
+                    </div>
+                    <div class="col-4 home">
+                        <a href="student.php" class="home-link"><i class="fa-solid fa-house"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!------End navbar------>
+
+    <!-----main content----->
+    <div class="bg-signup">
+        <img src="background-signup.jpg" alt="cvsucover">
+    </div>
+    <div class="signup-section signup">
+        <div class="container signup_container">
+            <div class="row">
+                <div class="col text-align ">
+                    <img src=logo.png>
+                    <div class="body_logo">
+                        <p> CAVITE STATE UNIVERSITY</p>
+                    </div>
+                    <div class="body-title">
+                        <h1>IMUS CAMPUS</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----signup section----->
+    <aside class="form-signup">
+        <div class="container">
+            <div class="box">
+                <div class="form-group">
+                    <div class="login-title">
+                        <h3>CREATE ACCOUNT</h3>
+                    </div>
+                    <!----F O R M---->
+                    <form method="POST">
+                        <div class="form-control">
+                            <label class="enter" for="name">Name:</label>
+
+                            <input type="text" class="form-control" id='name' name="name">
+                            <i class="fas fa-check-circle"></i>
+                            <i class="fas fa-exclamation-circle"></i>
+                            <small>Error</small>
+                        </div>
+                        <div class="form-control">
+                            <label for="id">Student ID:</label>
+
+                            <input type="text" id='id' class="form-control" name="id" required>
+                            <i class="fas fa-check-circle"></i>
+                            <i class="fas fa-exclamation-circle"></i>
+                            <small>Error</small>
+                        </div>
+                        <div class="form-control">
+                            <div class="box-control">
+                                <div class="form-control-1">
+                                    <label for="course">Course:</label>
+                                    <select name='course' id="course">
+                                        <?php foreach ($courses as $course): ?>
+                                            <option value="<?php echo $course['courseID']; ?>"><?php echo $course['courseName']; ?></option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+                                <div class="form-control-2">
+                                    <label for="section">Section:</label>
+                                    <select name="section" id="section">
+                                        <?php foreach ($sections as $section): ?>
+                                            <option value="<?php echo $section['sectionID']; ?>"><?php echo $course['sectionName']; ?></option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-control">
+                            <label for="password">Password:</label>
+
+                            <input type="password" class="form-control" id='password' name="password">
+                            <i class="fas fa-check-circle"></i>
+                            <i class="fas fa-exclamation-circle"></i>
+                            <small>Error</small>
+                        </div>
+                        <div class="form-control">
+                            <button class="btn btn-lg btn-block" id="btn" type="submit" value="Submit">SIGNUP</button>
+                        </div>
+                        <div class="login">
+                            <p>Already have an account? <a href="login.php"> Login now</a></p>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+        </div>
+    </aside>
+
+
+
+    <script>
+        
+        // JavaScript code to populate the section dropdown dynamically
+
+        // Define an object to store the sections for each course
+        var sections = {
+            <?php
+            // Fetch section data from the database based on courses
+            $query = "SELECT courseID, sectionName FROM tbl_sections";
+            $result = mysqli_query($conn, $query);
+            $sections = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+            // Organize the sections by course ID
+            $sectionsByCourse = [];
+            foreach ($sections as $section) {
+                $courseId = $section['courseID'];
+                $sectionName = $section['sectionName'];
+
+                if (!isset($sectionsByCourse[$courseId])) {
+                    $sectionsByCourse[$courseId] = [];
+                }
+
+                $sectionsByCourse[$courseId][] = $sectionName;
+            }
+
+            // Output the sections as JavaScript object
+            foreach ($sectionsByCourse as $courseId => $courseSections) {
+                echo "$courseId: [";
+                foreach ($courseSections as $index => $section) {
+                    if ($index !== 0) {
+                        echo ", ";
+                    }
+                    echo "'$section'";
+                }
+                echo "], ";
+            }
+            ?>
+        };
+
+        // Get reference to the section dropdown
+        var sectionDropdown = document.getElementById('section');
+
+        // Add an event listener to the course dropdown
+        document.getElementById('course').addEventListener('change', function () {
+            // Clear the section dropdown
+            sectionDropdown.innerHTML = '';
+
+            // Get the selected course ID
+            var selectedCourseId = this.value;
+
+            // Retrieve the sections for the selected course
+            var selectedCourseSections = sections[selectedCourseId];
+
+            // Populate the section dropdown based on the selected course
+            selectedCourseSections.forEach(function (section) {
+                var option = document.createElement('option');
+                option.textContent = section;
+                sectionDropdown.appendChild(option);
+            });
+        });
+
+        // Trigger the change event initially to populate the section dropdown
+        document.getElementById('course').dispatchEvent(new Event('change'));
+
+
+        // ------------------- JAVASCRIPT code for form validation -----------------------//
+
+        // JavaScript code for form validation
+
+        const form = document.querySelector('form');
+        const name = document.getElementById('name');
+        const id = document.getElementById('id');
+        const password = document.getElementById('password');
+
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            if (validate()) {
+                form.submit(); // Submit the form if validation succeeds
+            }
+        });
+
+        const namePattern = /^[a-zA-Z ]+$/;
+
+        function validate() {
+            const nameVal = name.value.trim();
+            const idVal = id.value.trim();
+            const passwordVal = password.value.trim();
+
+            let isValid = true; // Track the validation result
+
+            // Name validation
+            if (nameVal === '') {
+                setErrorMsg(name, 'Name cannot be blank');
+                isValid = false;
+
+
+            } else if (/\d/.test(nameVal)) {
+                setErrorMsg(name, 'Names should have no numeric characters');
+                isValid = false;
+
+            } else if (!namePattern.test(nameVal)) {
+                setErrorMsg(name, 'Names should only contain letters and spaces');
+                isValid = false;
+
+            } else {
+                setSuccessMsg(name);
+            }
+
+            // ID validation
+            if (idVal === '') {
+                setErrorMsg(id, 'ID cannot be blank');
+                isValid = false;
+            } else if (!/^\d+$/.test(idVal)) {
+                setErrorMsg(id, 'ID should only contain numbers');
+                isValid = false;
+            } else if (idVal.length !== 9) {
+                setErrorMsg(id, 'ID should have exactly 9 digits');
+                isValid = false;
+            } else {
+                setSuccessMsg(id);
+            }
+
+            // Password validation
+            if (passwordVal === '') {
+                setErrorMsg(password, 'Password cannot be blank');
+                isValid = false;
+            } else if (passwordVal.length < 8) {
+                setErrorMsg(password, 'Password should have at least 8 characters');
+                isValid = false;
+            } else if (!/[A-Z]/.test(passwordVal)) {
+                setErrorMsg(password, 'Password should contain at least 1 uppercase letter');
+                isValid = false;
+            } else if (!/[a-z]/.test(passwordVal)) {
+                setErrorMsg(password, 'Password should contain at least 1 lowercase letter');
+                isValid = false;
+            } else if (!/\d/.test(passwordVal)) {
+                setErrorMsg(password, 'Password should contain at least 1 numeric character');
+                isValid = false;
+            } else {
+                setSuccessMsg(password);
+            }
+
+            return isValid; // Return the overall validation result
+        }
+
+
+        function setErrorMsg(input, errorMsg) {
+            const formControl = input.parentElement;
+            const small = formControl.querySelector('small');
+            formControl.className = 'form-control error';
+            small.innerText = errorMsg;
+            small.style.display = 'block'; // Change visibility to display
+        }
+
+        function setSuccessMsg(input) {
+            const formControl = input.parentElement;
+            formControl.className = 'form-control success';
+            const small = formControl.querySelector('small');
+            small.style.display = 'none'; // Change visibility to display
+        }
+    </script>
+</body>
+
+</html>
