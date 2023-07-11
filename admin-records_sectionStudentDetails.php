30a6b1d65877bf546910dcea71c3dd26e6ed9293
Janvier Erickson <janviererickson.araque@cvsu.edu.ph>
2023-07-12 00:45:16 +0800

    details

--- admin-records_sectionStudentDetails.php
+++ admin-records_sectionStudentDetails.php
@@ -63,6 +63,7 @@ if ($result->num_rows > 0) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -70,19 +71,25 @@ if ($result->num_rows > 0) {
 
     <!----------TITLE------------>
     <link rel="shortcut icon" href="logo.png" type="">
-    <title><?php echo "Coordinator Page"; ?></title>
+    <title>
+        <?php echo "Coordinator Page"; ?>
+    </title>
 
-     <!----------CSS------------>
+    <!----------CSS------------>
     <link rel="stylesheet" href="style_admin.css">
 
-      <!----------BOOTSTRAP------------>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-     
-     <!----------FONTS------------>
+    <!----------BOOTSTRAP------------>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!----------FONTS------------>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Abel&family=Inter:wght@400;800&family=Poppins:wght@400;500&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Abel&family=Inter:wght@400;800&family=Poppins:wght@400;500&display=swap"
+        rel="stylesheet">
 
     <!----------ICONS------------>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -93,17 +100,18 @@ if ($result->num_rows > 0) {
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
 
-     
-<!---Inner topbar--->
-<?php include('topbar.php');?>
+
+    <!---Inner topbar--->
+    <?php include('topbar.php'); ?>
 
 </head>
 
 <!----Body----->
+
 <body>
-<section class="bg-section">
-   <!---------Sidebar------------>
-        <?php include('sidebar-admin.php');?>
+    <section class="bg-section">
+        <!---------Sidebar------------>
+        <?php include('sidebar-admin.php'); ?>
 
         <!--Main Content-->
         <main class="pcoded-main-content">
@@ -113,61 +121,71 @@ if ($result->num_rows > 0) {
 
                         <!--------------------------------- DIV PARA SA TITLE AT GO BACK BUTTON ----------------------------------->
                         <div class="upperbox">
-                        <h3><?php echo $selectedCourseName; ?></h3>
-                            
+                            <h3>
+                                <?php echo $selectedCourseName; ?>
+                            </h3>
+
                             <div class="searchbox">
-                            <input  type="text" id="searchInput" onkeyup="searchTable()" placeholder="Search student name...">
+                                <i class="fa-solid fa-magnifying-glass"></i><input type="text" class="form-control"
+                                    aria-label="Text input with checkbox" id="searchInput" onkeyup="searchTable()"
+                                    placeholder="Search section names...">
                             </div>
 
                             <div class="print">
-                            <button onclick="window.print()"> Print</button>
+                                <i class="fa-solid fa-print" onclick="window.print()"></i>
                             </div>
 
-                            <a href="admin-records_studentClasses.php" class="go-back-button"><ion-icon
+                            <a href="admin-records_Student.php" class="go-back-button"><ion-icon
                                     name="arrow-back-circle-outline"></ion-icon></a>
                         </div>
 
 
-                                    <!--------------------------------- TABLE FOR STUDENTS ----------------------------------->
+                        <!--------------------------------- TABLE FOR STUDENTS ----------------------------------->
+                        <div class="align-tbl-student">
+                            <div class="student-tbl-display">
+                                <div class="course-and-section">
+                                    <p>
+                                        <?php echo $selectedCourseName . ' - ' . $selectedsectionName; ?>
+                                    </p>
+                                </div>
 
-                        <div class="tabledisplay">
-                            <div class="course-and-section">
-                                <p><?php echo $selectedCourseName . ' - ' . $selectedsectionName; ?></p>
-                            </div>
-                            
-                            <table id="course-table">
-                                <thead>
-                                    <tr>
-                                        <th>Student Names</th>
-                                    </tr>
-                                </thead>
-                                <tbody class="scrollable-tbody">
-                                    <?php if (!empty($students)): ?>
+                                <table id="student-course-table">
+                                    <thead>
+                                        <tr>
+                                            <th>Student Names</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="scrollable-tbody">
+                                        <?php if (!empty($students)): ?>
                                         <?php foreach ($students as $student): ?>
-                                            <tr>
-                                                <td>
-                                                <a href="studentData.php?id=<?php echo $student['id']; ?>">
-                                                        <?php echo $student['name']; ?>
-                                                    </a>
-                                                </td>
-                                            </tr>
+                                        <tr>
+                                            <td>
+                                                <a
+                                                    href="admin-records_studentData.php?id=<?php echo $student['id']; ?>">
+                                                    <?php echo $student['name']; ?>
+                                                </a>
+                                            </td>
+                                        </tr>
                                         <?php endforeach; ?>
-                                    <?php else: ?>
+                                        <?php else: ?>
                                         <tr>
                                             <td colspan="4">No students found.</td>
                                         </tr>
-                                    <?php endif; ?>
-                                </tbody>
-                            </table>
+                                        <?php endif; ?>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
+
                     </div>
-                    </div>
-                    </div>
+                </div>
             </div>
-        </main>        
-        <!-----End Main content------>        
-        
-<!-----End of Body------>
-</section>
+        </main>
+
+        <!-----End Main content------>
+
+        <!-----End of Body------>
+    </section>
 </body>
+
 </html>
