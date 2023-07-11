3a47295c688107077d5f59e3e15c1e32099f5c4d
Maezelle Secuya <maezelle.secuya@cvsu.edu.ph>
2023-07-12 00:31:40 +0800

    instructor-stud-info and admin-stud-data 7/12/23

--- admin-records_studentData.php
+++ admin-records_studentData.php
@@ -1,7 +1,7 @@
 <?php
-include 'topbar.php';
+
 include 'db_connect.php';
-include 'sidebar.php';
+
 
 $studentName = '';
 $sectionName = '';
@@ -19,146 +19,155 @@ if (isset($_GET['id'])) {
 }
 
 
-    // Retrieve student details from the database
-    $query = "SELECT student.name, tbl_sections.sectionName, tbl_course.courseName, student.id
+// Retrieve student details from the database
+$query = "SELECT student.name, tbl_sections.sectionName, tbl_course.courseName, student.id
               FROM student
               INNER JOIN tbl_sections ON student.sectionID = tbl_sections.sectionID
               INNER JOIN tbl_course ON tbl_sections.courseID = tbl_course.courseID
-              WHERE student.id = '$studentID'"; 
+              WHERE student.id = '$studentID'";
 
 
 
 
-    $result = mysqli_query($conn, $query);
+$result = mysqli_query($conn, $query);
 
-    if ($result && mysqli_num_rows($result) > 0) {
-        // Fetch the student details
-        $student = mysqli_fetch_assoc($result);
+if ($result && mysqli_num_rows($result) > 0) {
+    // Fetch the student details
+    $student = mysqli_fetch_assoc($result);
 
-        // Retrieve the individual values
-        $studentName = $student['name'];
-        $sectionName = $student['sectionName'];
-        $courseName = $student['courseName'];
-        $studentID = $student['id'];
-    } else {
-        echo "No student found with the provided ID.";
-    }
+    // Retrieve the individual values
+    $studentName = $student['name'];
+    $sectionName = $student['sectionName'];
+    $courseName = $student['courseName'];
+    $studentID = $student['id'];
+} else {
+    echo "No student found with the provided ID.";
+}
 
 
 ?>
 
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <style>
-        /* CSS styles for the table display */
-        .content {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            padding-top: 20px;
-            position: relative;
-        }
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        .upperbox {
-            border-bottom: solid black 1px;
-            height: 50px;
-            width: 100%;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            margin-bottom: 20px;
-        }
+    <!----------TITLE------------>
+    <link rel="shortcut icon" href="logo.png" type="">
+    <title>
+        <?php echo "Coordinator Page"; ?>
+    </title>
 
-        .searchbox {
-            display: flex;
-            align-items: center;
-            margin-left: auto; /* Adjust the margin as needed */
-            margin-right: 20px; /* Adjust the margin as needed */
-        }
+    <!----------CSS------------>
+    <link rel="stylesheet" href="style_admin.css">
 
-        .searchbox input[type="text"] {
-            width: 300px;
-            padding: 8px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            font-size: 14px;
-            height: 38px;
-        }
+    <!----------BOOTSTRAP------------>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-        .print button {
-            background-color: white;
-            border: 1px solid black;
-            border-radius: 3px;
-            padding: 5px 10px;
-            color: black;
-            cursor: pointer;
-            height: 38px;
-            margin-right: 15px;
-        }
+    <!----------FONTS------------>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Abel&family=Inter:wght@400;800&family=Poppins:wght@400;500&display=swap"
+        rel="stylesheet">
 
-        .upperbox .go-back-button {
-            padding: 10px 20px;
-            height: 20px;
-            background-color: #4CAF50;
-            color: #fff;
-            text-decoration: none;
-            border-radius: 3px;
-        }
+    <!----------ICONS------------>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script src="https://kit.fontawesome.com/11a4f2cc62.js" crossorigin="anonymous"></script>
 
-        .upperbox .go-back-button:hover {
-            background-color: #45a049;
-        }
+    <!----------ALERTS-------------->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
-        .middlebox {
-            width: 1050px;
-            border: 1px solid black;
-            height: 500px;
-            padding: 10px;
-            margin-left: 30px;
-        }
-    </style>
+
+
+
+    <!---Inner topbar--->
+    <?php include('topbar.php'); ?>
+
 </head>
+
+<!----Body----->
+
 <body>
+    <section class="bg-section">
+        <!---------Sidebar------------>
+        <?php include('sidebar-admin.php'); ?>
 
-<div class="content print-container">
-
-
-    <div class="upperbox">
-        <h3>Student Records</h3>
-        <div class="searchbox">
-            <input type="text" id="searchInput" onkeyup="searchTable()" placeholder="Search section names...">
-        </div>
-        <div class="print">
-            <button onclick="window.print()">Print</button>
-        </div>
-        <a href="sectionStudentDetails.php" class="go-back-button">Go Back</a>
-    </div>
+        <!--Main Content-->
+        <main class="pcoded-main-content">
+            <div class="container">
+                <div class="col-lg-12">
+                    <div class="rec-content print-container">
 
 
-
-    <div class="middlebox">
-
-     <!----------------------------- SHOWING STUDENT DETAILS ------------------------>
-
-     
-        <?php if (!empty($studentName) && !empty($sectionName) && !empty($courseName) && !empty($studentID)) : ?>
-            <h3>PERSONAL DETAILS</h3>
-            <p>Full Name: <?php echo $studentName ?></p>
-            <p>NSTP Program: <?php echo $courseName ?></p>
-
-            <h3>ACADEMIC DETAILS</h3>
-            <p>Course and Section: <?php echo $sectionName ?></p>
-            <p>Student Number: <?php echo $studentID ?></p>
-        <?php else : ?>
-            <p>No student data available.</p>
-        <?php endif; ?>
+                        <div class="upperbox">
+                            <h3>Student Records</h3>
+                            <div class="searchbox">
+                                <i class="fa-solid fa-magnifying-glass"></i><input type="text" class="form-control"
+                                    aria-label="Text input with checkbox" id="searchInput" onkeyup="searchTable()"
+                                    placeholder="Search section names...">
+                            </div>
+                            <div class="print">
+                                <i class="fa-solid fa-print" onclick="window.print()"></i>
+                            </div>
+                            <a href="admin-records_sectionStudentDetails.php" class="go-back-button"><ion-icon
+                                    name="arrow-back-circle-outline"></ion-icon></a>
+                        </div>
 
 
+                        <div class="student-info">
+                            <div class="middle-box_Student">
 
-    </div>
-</div>
+                                <!----------------------------- SHOWING STUDENT DETAILS ------------------------>
 
+
+                                <?php if (!empty($studentName) && !empty($sectionName) && !empty($courseName) && !empty($studentID)): ?>
+                                    <h3>PERSONAL DETAILS</h3>
+                                    <p>Full Name:
+                                        <b>
+                                            <?php echo $studentName ?>
+                                        </b>
+                                    </p>
+                                    <p>NSTP Program:
+                                        <b>
+                                            <?php echo $courseName ?>
+                                        </b>
+                                    </p>
+
+                                    <h3>ACADEMIC DETAILS</h3>
+                                    <p>Course and Section:
+                                        <b>
+                                            <?php echo $sectionName ?>
+                                        </b>
+                                    </p>
+                                    <p>Student Number:
+                                        <b>
+                                            <?php echo $studentID ?>
+                                        </b>
+                                    </p>
+                                <?php else: ?>
+                                    <p>No student data available.</p>
+                                <?php endif; ?>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <!-----End Main content------>
+
+        <!-----End of Body------>
+    </section>
 </body>
+
 </html>
---
+++ instructor-student_info.php
@@ -0,0 +1,101 @@
+<?php
+
+include 'db_connect.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!----------TITLE------------>
+    <link rel="shortcut icon" href="logo.png" type="">
+    <title>
+        <?php echo "Coordinator Page"; ?>
+    </title>
+
+    <!----------CSS------------>
+    <link rel="stylesheet" href="instructor_style.css">
+
+    <!----------BOOTSTRAP------------>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!----------FONTS------------>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Abel&family=Inter:wght@400;800&family=Poppins:wght@400;500&display=swap"
+        rel="stylesheet">
+
+    <!----------ICONS------------>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script src="https://kit.fontawesome.com/11a4f2cc62.js" crossorigin="anonymous"></script>
+
+    <!----------ALERTS-------------->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+
+
+
+    <!---Inner topbar--->
+    <?php include('topbar.php'); ?>
+
+</head>
+
+<!----Body----->
+
+<body>
+    <section class="bg-section">
+        <!---------Sidebar------------>
+        <?php include('sidebar-instructor.php'); ?>
+
+        <!----Main Content Instructor Student Info------>
+        <div class="pcoded-main-content">
+            <div class="container pt-4">
+                <div class="col-lg-12">
+                    <div class="col-lg-8">
+                        <div class="active-box">
+                            <h5>STUDENT INFO.</h5>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="instructor-classes.html">
+                            <ion-icon name="arrow-back-circle-outline"></ion-icon>
+                        </a>
+                    </div>
+                    <div></div>
+                    <hr class="main-content-line">
+                </div>
+                <div class="col-md-9 student-info">
+                    <div class="stud-details">
+                        <h2>Bernard Fernandez</h2>
+                        <p>Bachelor of Science in Information Technology</p>
+                        <p>First Year</p>
+                        <p>202110118</p>
+                        <p>CWTS</p>
+                        <h2>Contact</h2>
+                        <p>09123456789</p>
+                        <p>Fernandez.Bernard@Cvsu.edu.ph</p>
+                        <h2>Nstp Instructor :</h2>
+                        <p>Armand Aton</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        </div>
+
+    </section>
+
+</body>
+
+</html>
