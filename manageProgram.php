389e8adf0087c33c1c770004e9e34ac7e025268e
mrknthny21 <136783891+mrknthny21@users.noreply.github.com>
2023-08-27 16:40:20 +0800

    not finished

--- manageProgram.php
+++ manageProgram.php
@@ -2,60 +2,57 @@
 include 'db_connect.php';
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Check if it's a delete operation
-    if (isset($_POST['programID']) && isset($_POST['deleteProgram'])) {
-        $programID = $_POST['programID'];
+    if (isset($_POST['deleteProgram'])) {
+        // Delete operation
+        if (isset($_POST['programID'])) {
+            $programID = $_POST['programID'];
+            $deleteQuery = "DELETE FROM tbl_program WHERE programID = '$programID'";
+            $conn->query($deleteQuery);
+            header('Location: admin-createprogram.php');
+            exit();
+        }
+    } elseif (isset($_POST['updateProgram'])) {
+        // Update operation
+        if (isset($_POST["updateProgramID"]) && isset($_POST["programName"]) && isset($_POST["programLocation"]) && isset($_POST["description"]) && isset($_POST["instructor_id"]) && isset($_POST["scheduleDate"]) && isset($_POST["start_time"]) && isset($_POST["end_time"])) {
+            $programID = $_POST["updateProgramID"];
+            $programName = $_POST["programName"];
+            $programLocation = $_POST["programLocation"];
+            $description = $_POST["description"];
+            $instructorID = $_POST["instructor_id"];
+            $scheduleDate = $_POST["scheduleDate"];
+            $start_time = $_POST["start_time"];
+            $end_time = $_POST["end_time"];
 
-        // Delete the program from the database
-        $deleteQuery = "DELETE FROM tbl_program WHERE programID = '$programID'";
-        $conn->query($deleteQuery);
+            $sql = "UPDATE tbl_program SET programName = ?, programLocation = ?, description = ?, instructorID = ?, scheduleDate = ?, start_time = ?, end_time = ? WHERE programID = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sssiisss", $programName, $programLocation, $description, $instructorID, $scheduleDate, $start_time, $end_time, $programID);
+            $stmt->execute();
+            $stmt->close();
 
-        // Redirect back to the page to refresh the program list
-        header('Location: admin-createprogram.php');
-        exit();
-    }
+            header("Location: admin-createprogram.php");
+            exit();
+        }
+    } elseif (isset($_POST['addProgram'])) {
+        // Add operation
+        if (isset($_POST["programName"]) && isset($_POST["programLocation"]) && isset($_POST["description"]) && isset($_POST["instructor_id"]) && isset($_POST["scheduleDate"]) && isset($_POST["start_time"]) && isset($_POST["end_time"])) {
+            $programName = $_POST["programName"];
+            $programLocation = $_POST["programLocation"];
+            $description = $_POST["description"];
+            $instructorID = $_POST["instructor_id"];
+            $scheduleDate = $_POST["scheduleDate"];
+            $scheduleDate = date('Y-m-d', strtotime($scheduleDate)); // Format the date
+            $start_time = $_POST["start_time"];
+            $end_time = $_POST["end_time"];
 
-    // Check if it's an edit operation
-    else if (isset($_POST["programID"]) && isset($_POST["programName"]) && isset($_POST["programLocation"]) && isset($_POST["description"]) && isset($_POST["instructor_id"]) && isset($_POST["scheduleDate"]) && isset($_POST["start_time"]) && isset($_POST["end_time"])) {
-        $programID = $_POST["programID"];
-        $programName = $_POST["programName"];
-        $programLocation = $_POST["programLocation"];
-        $description = $_POST["description"];
-        $instructorID = $_POST["instructor_id"];
-        $scheduleDate = $_POST["scheduleDate"];
-        $start_time = $_POST["start_time"];
-        $end_time = $_POST["end_time"];
+            $sql = "INSERT INTO tbl_program (programName, programLocation, description, instructorID, scheduleDate, start_time, end_time) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sssiiss", $programName, $programLocation, $description, $instructorID, $scheduleDate, $start_time, $end_time);
+            $stmt->execute();
+            $stmt->close();
 
-        // Perform your database update logic here
-        $sql = "UPDATE tbl_program SET programName = ?, programLocation = ?, description = ?, instructorID = ?, scheduleDate = ?, start_time = ?, end_time = ? WHERE programID = ?";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("sssiisss", $programName, $programLocation, $description, $instructorID, $scheduleDate, $start_time, $end_time, $programID);
-        $stmt->execute();
-        $stmt->close();
-
-        header("Location: editProgram.php?programID=".$programID);
-        exit();
-    }
-
-    // Check if it's an add operation
-    else if (isset($_POST["programName"]) && isset($_POST["programLocation"]) && isset($_POST["description"]) && isset($_POST["instructor_id"]) && isset($_POST["scheduleDate"]) && isset($_POST["start_time"]) && isset($_POST["end_time"])) {
-        $programName = $_POST["programName"];
-        $programLocation = $_POST["programLocation"];
-        $description = $_POST["description"];
-        $instructorID = $_POST["instructor_id"];
-        $scheduleDate = $_POST["scheduleDate"];
-        $start_time = $_POST["start_time"];
-        $end_time = $_POST["end_time"];
-
-        // Perform your database insertion logic here
-        $sql = "INSERT INTO tbl_program (programName, programLocation, description, instructorID, scheduleDate, start_time, end_time) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("sssiiss", $programName, $programLocation, $description, $instructorID, $scheduleDate, $start_time, $end_time);
-        $stmt->execute();
-        $stmt->close();
-
-        header("Location: admin-createProgram.php");
-        exit();
+            header("Location: admin-createProgram.php");
+            exit();
+        }
     }
 }
 ?>
